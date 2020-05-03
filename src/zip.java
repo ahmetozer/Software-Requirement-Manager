4ce06d18a467004eda90cc8ee7fed4b34c60acce
Ahmet <10574489+AhmetOZER@users.noreply.github.com>
2020-05-03 03:14:44 +0300

    Pure zip function has a issue with windows. Function replaced with os execution

--- src/zip.java
+++ src/zip.java
@@ -1,7 +1,9 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -10,48 +12,51 @@ public class zip{
 		System.out.println("UNZIP: Zip file directory is not defined. Please give a zip file");
 	}
 	static void unzip(String zipFilePath) {
-		file_functions  myHomePage = new file_functions (zipFilePath);
-		System.out.println("UNZIP: output directory is not defined. Using default path" + myHomePage.path());
-		unzip(zipFilePath, myHomePage.path()); // Calling unzip recursive with giving dest dir.
-		
+		System.out.println("UNZIP: Zip output directory is not defined. So files extracted to /get folder");
+		unzip(zipFilePath, "/get");
 	}
-	static void unzip(String zipFilePath, String destDir) {
-		File dir = new File(destDir);
-		// create output directory if it doesn't exist
-		if(!dir.exists()) dir.mkdirs();
-		FileInputStream fis;
-		//buffer for read and write data to file
-		byte[] buffer = new byte[1024];
+
+	static void unzip(String zipFilePath, String path)  {
+		//file_functions  myHomePage = new file_functions (zipFilePath);
+		//System.out.println("UNZIP: output directory is not defined. Using default path" + myHomePage.path());
+		//unzip(zipFilePath, myHomePage.path()); // Calling unzip recursive with giving dest dir.
+		String command = "powershell -Command Expand-Archive -Force "+zipFilePath+" " + path;
+		if (OS_Detection.getOperatingSystemType().toString() == "Linux") {
+			command = "unzip "+zipFilePath+" " + path;
+		  } else if (OS_Detection.getOperatingSystemType().toString() == "Windows") {
+			  command = "powershell -Command Expand-Archive -Force "+zipFilePath+" " + path;
+		  } else {
+			  System.out.println("Error : OS Type not supported");
+			  return;
+		  }
+		
+		 
 		try {
-			fis = new FileInputStream(zipFilePath);
-			ZipInputStream zis = new ZipInputStream(fis);
-			ZipEntry ze = zis.getNextEntry();
-			while(ze != null){
-				String fileName = ze.getName();
-				File newFile = new File(destDir + File.separator + fileName);
-				System.out.println("Unzipping to "+newFile.getAbsolutePath());
-				//create directories for sub directories in zip
-				new File(newFile.getParent()).mkdirs();
-				FileOutputStream fos = new FileOutputStream(newFile);
-				int len;
-				while ((len = zis.read(buffer)) > 0) {
-					fos.write(buffer, 0, len);
-				}
-				fos.close();
-				//close this ZipEntry
-				zis.closeEntry();
-				ze = zis.getNextEntry();
-			}
-			//close last ZipEntry
-			zis.closeEntry();
-			zis.close();
-			fis.close();
+			Process process = Runtime.getRuntime().exec(command);
+			// deal with OutputStream to send inputs
+			process.getOutputStream();
+			 
+			// deal with InputStream to get ordinary outputs
+			process.getInputStream();
+			 
+			// deal with ErrorStream to get error outputs
+			process.getErrorStream();
+		    BufferedReader reader = new BufferedReader(
+		            new InputStreamReader(process.getInputStream()));
+		    String line;
+		    while ((line = reader.readLine()) != null) {
+		        System.out.println(line);
+		    }
+		 
+		    reader.close();
+		 
 		} catch (IOException e) {
-			e.printStackTrace();
+		    e.printStackTrace();
 		}
 
 	}
 	
+
 	/* TEST FUNCTION
 	public static void main(String[] args) {
 		String zipFilePath = "/Users/ahmet/Desktop/tmp.zip";
